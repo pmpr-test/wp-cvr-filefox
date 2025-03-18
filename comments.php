b76ef6d339e04d020ea5b3b576e424cd207bb977
pmpr.dev <pmpr.dev@gmail.com>
2025-03-18 20:14:20 +0000

    Obfuscated

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             67cf387269310             |
+    |             67d9d3e47b928             |
     |_______________________________________|
 */
  pmpr_do_action('render_comments');
--- src/Pmpr/Cover/Filefox/Woocommerce/Common.php
+++ src/Pmpr/Cover/Filefox/Woocommerce/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             67cf387269310             |
+    |             67d9d3e47b928             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox\Woocommerce; use Pmpr\Cover\Filefox\Container; abstract class Common extends Container { }
