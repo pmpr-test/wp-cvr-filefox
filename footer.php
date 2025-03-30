bbabdc01c2257d4b9de3c43426477a8360e0af3c
pmpr.dev <pmpr.dev@gmail.com>
2025-03-30 12:08:34 +0000

    Obfuscated

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             67e92d0a5e4e0             |
+    |             67e93408155f5             |
     |_______________________________________|
 */
  pmpr_do_action('render_footer');
