10f3b612f0c6250b56097c439cd3899c9e7d039d
pmpr.dev <pmpr.dev@gmail.com>
2025-05-03 22:53:21 +0000

    Obfuscated

--- src/Pmpr/Cover/Filefox/Container.php
+++ src/Pmpr/Cover/Filefox/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68169d894d50c             |
+    |             68169e2758346             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Cover\Filefox\Traits\CommonTrait; abstract class Container extends BaseClass { use CommonTrait; }
--- src/Pmpr/Cover/Filefox/Woocommerce/Common.php
+++ src/Pmpr/Cover/Filefox/Woocommerce/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68169d894d50c             |
+    |             68169e2758346             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox\Woocommerce; use Pmpr\Cover\Filefox\Container; abstract class Common extends Container { }
