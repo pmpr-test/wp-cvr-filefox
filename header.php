b42f2b6505560bf1a4dadc4f922192727c2fa6d5
pmpr.dev <pmpr.dev@gmail.com>
2025-05-23 01:11:42 +0000

    Obfuscated

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             682fca96200ca             |
+    |             682fcb152cf0d             |
     |_______________________________________|
 */
  pmpr_do_action('render_header');
--- src/Pmpr/Cover/Filefox/Woocommerce/Common.php
+++ src/Pmpr/Cover/Filefox/Woocommerce/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             682fca96200ca             |
+    |             682fcb152cf0d             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox\Woocommerce; use Pmpr\Cover\Filefox\Container; abstract class Common extends Container { }
