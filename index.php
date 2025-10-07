fe2841817a7111224af242c744e19291c4226758
pmpr.dev <pmpr.dev@gmail.com>
2025-10-07 22:45:12 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68e585e9be18d             |
+    |             68e597b9016c9             |
     |_______________________________________|
 */
  pmpr_do_action('init_cover');
--- src/Pmpr/Cover/Filefox/Page/Material/About.php
+++ src/Pmpr/Cover/Filefox/Page/Material/About.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68e585e9be18d             |
+    |             68e597b9016c9             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Filefox\Page\Material; use Pmpr\Common\Foundation\Frontend\TaxonomyEndPoint; use Pmpr\Common\Foundation\Interfaces\Constants; class About extends TaxonomyEndPoint { public function qiccuiwooiquycsg() { $this->ykuiyscecakwkmui(Constants::oeeqisiiqoswqqmy)->gecckeoacskeskii(__('About', PR__CVR__FILEFOX)); parent::qiccuiwooiquycsg(); } }
